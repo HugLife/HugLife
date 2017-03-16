98154ddd6d7d0234def5c5b465685737d4a6c768
Joseph Zizzo <zizzo.joseph.k@gmail.com>
2017-03-16 15:08:43 -0700

    add client. Connect client, server, and database

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+
+    return (
+      <div>
+        Bar List
+      </div>
+    )
+  }
+}
+
+export default App;
