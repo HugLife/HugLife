4a78f2285055e033477c914771a5ddfb8ae4716f
Dario Arteaga <daartv@gmail.com>
2017-03-20 18:04:47 -0700

    Made the config file for Mysql and the models file for the server/database basic interactions

---
+++ database-mysql/models.js
@@ -0,0 +1,53 @@
+var mysql = require('mysql');
+var mysqlConfig = require('./config.js');
+
+var connection = mysql.createConnection(mysqlConfig);
+
+var selectThreeBars = function(callback) {
+  connection.query('SELECT * FROM bars LIMIT 3', function (err, results, fields) {
+	if (err) {
+	  console.log('errored out from getting bars on models.js');
+	  callback(err, null);
+	} else {
+	  callback(null, results);
+	}
+  });
+};
+
+var selectOneBar = function (id, callback) {
+  connection.query(`SELECT * FROM bars WHERE bars.id = '${id}'`, function (err, results, fields) {
+  	if (err) {
+  	  console.log('errored out from getting one bar on models.js');
+  	  callback(err, null);
+  	} else {
+  	  callback(null, results);
+  	}
+  });
+};
+
+var selectOneBartender = function (barid, btname, callback) {
+  connection.query(`SELECT * FROM bartenders WHERE bartenders.bar_id = '${barid}' AND bartenders.name = '${btname}'`, function (err, results, fields) {
+  	if (err) {
+  	  console.log('errored out from getting one bartender on models.js');
+  	  callback(err, null);
+  	} else {
+  	  callback(null, results);
+  	}
+  });
+};
+
+var selectAllBartendersFromBar = function (barid, callback) {
+  connection.query(`SELECT * FROM bartenders WHERE bartenders.bar_id = '${barid}'`, function (err, results, fields) {
+  	if (err) {
+  	  console.log('errored out from getting all bartenders from one bar');
+  	  callback(err, null);
+  	} else {
+  	  callback(null, results);
+  	}
+  });
+};
+
+module.exports.selectThreeBars = selectThreeBars;
+module.exports.selectOneBar = selectOneBar;
+module.exports.selectOneBartender = selectOneBartender;
+module.exports.selectAllBartendersFromBar = selectAllBartendersFromBar;
