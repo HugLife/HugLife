ad7f6010904b8934795718c0dd177cd5faf493dc
Dario Arteaga <daartv@gmail.com>
2017-03-22 16:23:19 -0700

    Cleaned up schema code

--- schema.sql
+++ schema.sql
@@ -28,12 +28,12 @@ CREATE TABLE bars (
  	PRIMARY KEY (ID)
  	);
 
-  INSERT INTO bars (id, name, address, hours, description, phonenumber) VALUES (1, 'Tempest', '431 Natoma St', '11:00 am - 2:00 am', 'Nice bar on Natoma St', '555-555-5555');
-  INSERT INTO bars (id, name, address, hours, description, phonenumber) VALUES (2, 'Databases', '934 Howard St', '11:00 am - 2:00 am', 'A place to be yourself', '444-444-4444');
-  INSERT INTO bars (id, name, address, hours, description, phonenumber) VALUES (3, 'Mikeller', '34 Mason St', '11:00 am - 2:00 am', 'Hack Reactors students are our source of income', '333-333-3333');
+  INSERT INTO bars (id, name, address, hours, description, phonenumber) VALUES (1, 'Tempest', '431 Natoma St', '11:00 am - 2:00 am', 'Nice bar on Natoma St', '555-555-5555'),
+  (2, 'Databases', '934 Howard St', '11:00 am - 2:00 am', 'A place to be yourself', '444-444-4444'),
+  (3, 'Mikeller', '34 Mason St', '11:00 am - 2:00 am', 'Hack Reactors students are our source of income', '333-333-3333');
 
 
-  INSERT INTO bartenders (name, bar_id, expert_right, expert_left, friendly_right, friendly_left, quick_right, quick_left) VALUES ('Datum Bass', 2, 10, 5, 8, 4, 9, 1);
-  INSERT INTO bartenders (name, bar_id, expert_right, expert_left, friendly_right, friendly_left, quick_right, quick_left) VALUES ('Esteban Quito', 1, 15, 3, 2, 17, 2, 7);
-  INSERT INTO bartenders (name, bar_id, expert_right, expert_left, friendly_right, friendly_left, quick_right, quick_left) VALUES ('Sum Rand Om Gai', 3, 1, 15, 8, 24, 9, 31);
-  INSERT INTO bartenders (name, bar_id, expert_right, expert_left, friendly_right, friendly_left, quick_right, quick_left) VALUES ('John Doe', 2, 101, 50, 80, 40, 90, 10);
+  INSERT INTO bartenders (name, bar_id, expert_right, expert_left, friendly_right, friendly_left, quick_right, quick_left) VALUES ('Datum Bass', 2, 10, 5, 8, 4, 9, 1),
+  ('Esteban Quito', 1, 15, 3, 2, 17, 2, 7),
+  ('Sum Rand Om Gai', 3, 1, 15, 8, 24, 9, 31),
+  ('John Doe', 2, 101, 50, 80, 40, 90, 10);
