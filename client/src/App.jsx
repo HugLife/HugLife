f5178aaa577a9e1f9d35802755b45baa0d5be5ac
smmakowski <makowski.sm@gmail.com>
2017-03-22 12:04:07 -0700

    Add Static map of current location

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -37,18 +37,72 @@ class App extends Component {
     })
   }
 
+  searchMap (location) {
+    
+  }
+
+  componentDidMount() { 
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+
+      var $img = $('img');
+      var url = 'https://maps.googleapis.com/maps/api/staticmap?' +
+                'key=AIzaSyBSJt0j19FiHBC4ExMsKCh7b7kujSNYImo' +
+                '&center=' + pos.lat + ',' + pos.lng + '&zoom=15' +
+                '&size=480x293';
+
+      $img.attr('src', url);
+
+
+
+
+
+      })
+    } else {
+          // $.ajax({
+    //   url: 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?',
+    //   data:{
+    //     key: 'AIzaSyDPnksoq1GqbY-SoDONZTHyj54ovde-2k8',
+    //     location: '37.7876,-122.4001',
+    //     radius: '500',
+    //     type: 'bar',
+    //     keyword: ''
+    //   },
+    //   contentType: 'jsonp',
+    //   beforeSend: function (request) {
+    //     request.setRequestHeader('Access-Control-Allow-Origin', '*');
+
+    //   },
+    //   success: function(data) {
+    //     console.log(data);
+    //   },
+    //   error: function(error) {
+    //     console.log(error);
+    //   }
+
+    // })
+    }
+
+
+  }
   render() {
 
     return (
       <div>
         <div>
-        <input type="text" value={this.state.value} onChange={this.handleChange.bind(this)} />
-        <button onClick={this.handleSubmit(this.state.value)}>Add Bar</button> 
+          <input type="text" value={this.state.value} onChange={this.handleChange.bind(this)} />
+          <button onClick={this.handleSubmit(this.state.value)}>Add Bar</button> 
         </div>
         <div>
-        <img src={'map.jpg'} style={divStyle} id="map" />
-        <h4>Bar List</h4>
-        {this.state.bars.map(bar => <li key={bar.key}>{bar.name}</li>)}
+          <img src={'map.jpg'} style={divStyle}/>
+          <h4>Bar List</h4>
+          {this.state.bars.map(bar => <li key={bar.key}>{bar.name}</li>)}
+          </div>
+        <div id="map">
         </div>
       </div>
     )
