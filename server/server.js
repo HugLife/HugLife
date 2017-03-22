acadda2c42c40209b1139eaf0297f73ba3a3612f
Dario Arteaga <daartv@gmail.com>
2017-03-21 21:13:09 -0700

    Added the controllers for the basic models and they work properly.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var drinkedin = require('../databse-mysql');
+var db = require('../database-mysql/models.js');
 
 var app = express();
 
@@ -9,15 +9,45 @@ app.use(express.static(__dirname + '/../client/dist'));
 app.use(bodyParser.json());
 
 
-app.get('/users', function (req, res) {
-  drinkd.selectAllUsers(function(err, data) {
-    if(err) {
-      res.sendStatus(500);
-    } else {
-      res.json(data);
-    }
+app.get('/barlist', function (req, res) {
+  db.selectThreeBars(function (error, result) {
+  	if (error) {
+  	  console.log('errored out /barlist', error);
+  	}
+  	console.log('This are the three bars', result);
+  	res.send(result);
   });
 });
+
+app.get('/bar', function (req, res) {
+  db.selectOneBar(1, function (error, result) {
+  	if (error) {
+  	  console.log('errored out /bar', error);
+  	}
+  	console.log('this is the bar', result);
+  	res.send(result);
+  })
+})
+
+app.get('/bartender', function (req, res) {
+	db.selectOneBartender(2, 'Datum Bass', function (error, result) {
+	  if (error) {
+	  	console.log('errored out /bartender', error);
+	  }
+	  console.log('this is the bartender', result);
+	  res.send(result);
+	})
+})
+
+app.get('/bartenderlist', function (req, res) {
+	db.selectAllBartendersFromBar(1, function (error, result) {
+	  if (error) {
+	  	console.log('errored out from /bartenderlist', error)
+	  }
+	  console.log('this is the bartender list', result);
+	  res.send(result);
+	})
+})
 
 app.listen(5000, function() {
   console.log('listening on port 5000!');
