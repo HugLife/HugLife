a0cb96add4a3dfc48a0d3377ee3df5c5ec066c38
Neil Vodoor <nvodoor@gmail.com>
2017-03-24 09:53:47 -0700

    Should re-render without causing massive data transfers when clicking "home" button.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -53,7 +53,117 @@ class App extends Component {
     })
   }
 
-  searchMap (location) {
+  componentDidUpdate() {
+    var context = this;
+    setInterval(function () { 
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+   
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: pos,
+          zoom: 17
+        });
+
+        var service = new google.maps.places.PlacesService(map);
+        service.nearbySearch({
+          location: pos,
+          types: ['bar'],
+          rankBy: google.maps.places.RankBy.DISTANCE
+
+
+        }, function(results, status, pagination) {
+            if (status !== google.maps.places.PlacesServiceStatus.OK) {
+              console.log('error')
+              return;
+            } else {
+
+            function addMarker(place) {
+              var marker = new google.maps.Marker({
+              map: map,
+              position: place.geometry.location,
+              label: '' + (i + 1)
+            });
+            }
+
+            function addHome(place) {
+              var marker = new google.maps.Marker({
+              map: map,
+              position: place,
+              label: {text: 'X', labelColor: 'green'}
+
+            });
+            }
+              console.log(results);
+              context.setState({
+                bars: results
+              });
+
+              
+
+              for (var i = 0; i < results.length; i++){
+                addMarker(results[i]);
+              }
+
+              addHome(pos);
+
+            }
+        }) 
+      }, function(){
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 37.7876, lng: -122.4001},
+          zoom: 17
+        });
+
+         var service = new google.maps.places.PlacesService(map);
+        service.nearbySearch({
+          location: {lat: 37.7876, lng: -122.4001},
+          types: ['bar'],
+          rankBy: google.maps.places.RankBy.DISTANCE
+
+
+        }, function(results, status, pagination) {
+            if (status !== google.maps.places.PlacesServiceStatus.OK) {
+              console.log('error')
+              return;
+            } else {
+
+            function addMarker(place) {
+              var marker = new google.maps.Marker({
+              map: map,
+              position: place.geometry.location,
+              label: '' + (i + 1)
+            });
+            }
+
+            function addHome(place) {
+              var marker = new google.maps.Marker({
+              map: map,
+              position: place.geometry.location,
+              label: 'X',
+              labelColor: 'green'
+            });
+            }
+              console.log(results);
+              context.setState({
+                bars: results
+              });
+
+              for (var i = 0; i < results.length; i++){
+                addMarker(results[i]);
+              }
+
+            }
+        }) 
+        alert('GEOLOCATION ACCESS DENIED: LOCATION DEFAULTED TO HACK REACTOR SF');
+      })
+    } else {
+      // do nothing
+    }}, 20000 )
+
   }
 
   componentDidMount() { 
