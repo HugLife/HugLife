d9f633459392af24c4175cda2520da17d0029379
Dario Arteaga <daartv@gmail.com>
2017-03-24 13:14:52 -0700

    Changed the type of the bar id to a varchar
    Google Places data has an ID of a string so it had to be changed for them to be added to the database correctly

--- schema.sql
+++ schema.sql
@@ -5,7 +5,7 @@ CREATE DATABASE drinkedin;
 USE drinkedin;
 
 CREATE TABLE bars (
-  id int NOT NULL UNIQUE,
+  id varchar(100) NOT NULL UNIQUE,
   name varchar(50) NOT NULL,
   address varchar(200) NOT NULL,
   PRIMARY KEY (ID)
@@ -14,7 +14,7 @@ CREATE TABLE bars (
  CREATE TABLE bartenders (
  	id int NOT NULL AUTO_INCREMENT UNIQUE,
  	name varchar(200) NOT NULL,
- 	bar_id int NOT NULL,
+ 	bar_id varchar(100) NOT NULL,
  	expert_right int,
  	expert_left int,
  	friendly_right int,
@@ -27,6 +27,6 @@ CREATE TABLE bars (
 
 
   INSERT INTO bartenders (name, bar_id, expert_right, expert_left, friendly_right, friendly_left, quick_right, quick_left) VALUES ('Datum Bass', 2, 10, 5, 8, 4, 9, 1),
-  ('Esteban Quito', 1, 15, 3, 2, 17, 2, 7),
-  ('Sum Rand Om Gai', 3, 1, 15, 8, 24, 9, 31),
-  ('John Doe', 2, 101, 50, 80, 40, 90, 10);
+  ('Esteban Quito', '1', 15, 3, 2, 17, 2, 7),
+  ('Sum Rand Om Gai', '3', 1, 15, 8, 24, 9, 31),
+  ('John Doe', '2', 101, 50, 80, 40, 90, 10);
