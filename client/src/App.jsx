ff2eaabaf89fec15313803d0b55a3cca4e0f4fe6
smmakowski <makowski.sm@gmail.com>
2017-03-24 00:37:27 -0700

    Fix git issues related to merge

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -25,10 +25,7 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      page: 'home',
-      value: 'Enter Bar',
-      bars: [{name: 'Tempest', key: 1}, {name: 'Databases', key: 2}, {name: 'Ol\'McDonalds', key: 3}],
-      currentBar: [{name:'Tempest', key: 1}]
+
     }
   }
 
@@ -38,31 +35,11 @@ class App extends Component {
   }
 
   render() {
-
     return (
       <div>
-<<<<<<< HEAD
-        <Header />
-        <div>
-          <input type="text" value={this.state.value} onChange={this.handleChange.bind(this)} />
-          <button onClick={this.handleSubmit(this.state.value)}>Add Bar</button> 
-        </div>
-        <div>
-          <h4>Bars Near You</h4>
-          <ol>
-            {this.state.bars.map(bar => <li key={bar.id}>{bar.name}</li>)}
-          </ol>
-          </div>
-        <div style={mapStyle} id="map">
-        </div>
-        * Numbers on map correspond to numbers on list
-        ** X on map is your current location
- 
-=======
         <BarList />
         <BarProfile />
         <BartenderProfile />
->>>>>>> a5e8e71fe016f9e2693e6e5cbaac3aa2f8af3bd5
       </div>
     )
   }
