a6ef261657856fed53f246b4016cc0e7713d8cbd
smmakowski <makowski.sm@gmail.com>
2017-03-26 12:14:46 -0700

    Add process.env.PORT for use with heroku deployment

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var db = require('../database-mysql/models.js');
 
 var app = express();
 
+var PORT = process.env.PORT || 5000;
 
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(bodyParser.json());
@@ -120,7 +121,7 @@ app.post('/rate', function (req, res) {
     }
   })
 })
-app.listen(5000, function() {
-  console.log('listening on port 5000!');
+app.listen(PORT, function() {
+  console.log('listening on port ' + PORT + '!');
 });
 
