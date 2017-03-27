d0492affe37e4c5243ccc231c4a85b07f5273da6
Dario Arteaga <daartv@gmail.com>
2017-03-27 10:30:24 -0700

    Login component

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -9,18 +9,24 @@ class Login extends React.Component {
     }
   }
 
-  onChange (e) {
+  userOnChange (e) {
     this.setState({
       user: e.target.value
     });
   }
 
+  passwordOnChange(e) {
+    this.setState({
+      password: e.target.value
+    })
+  }
+
   render() {
     return (<div className='page-header'>
       <h4>Username:</h4>
-      <input value={this.state.terms} onChange={this.onChange.bind(this)}/>  
+      <input value={this.state.user} onChange={this.userOnChange.bind(this)}/>  
       <h4>Password:</h4>
-      <input value={this.state.terms} onChange={this.onChange.bind(this)}/>   
+      <input value={this.state.password} onChange={this.passwordOnChange.bind(this)}/>   
       <button className='btn btn-warning'> Sign in </button>
     </div>) 
   }
